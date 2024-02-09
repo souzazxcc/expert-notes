3585d0e0e0c7dc66fc911154e9c28ab1e9f0d4fd
souzazxcc <thilincolndev@gmail.com>
2024-02-08 21:54:39 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import logo from "./assets/logo.svg";
+import { NewNoteCard } from "./components/new-note";
+import { NoteCard } from "./components/note-card";
+
+interface Note {
+  id: String,
+  date: Date,
+  content: string
+}
+
+export function App() {
+  const [notes, setNotes] = useState<Note[]>([])
+
+  function onNoteCreated(content: string) {
+    const newNotes = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNotes, ...notes]
+
+    setNotes(notesArray)
+    
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+    
+  }
+
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW Expert" />
+
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+
+        { notes.map(note => {
+          return <NoteCard key={note.id} note={note} />
+        })}
+      </div>
+    </div>
+  );
+}
